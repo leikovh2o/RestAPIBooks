ea4d11d1623e5c835769c5917c10ddd8e2ffc88a
Ion <ioncazacu13@gmail.com>
2022-08-04 12:04:26 +0300

    FileUpdates

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-import booksRoutes from './books.js';
+import booksRoutes from './routes/books.js';
 
 const app = express();
 const PORT = 8080;
